19c48458b36f258933f3bf1707d5bb79bf0caee7
kevinkim910408 <kevinkim0408@naver.com>
2020-10-08 23:52:11 +0900

    add english comments
    add english comments

--- Assets/NetworkMan.cs
+++ Assets/NetworkMan.cs
@@ -10,15 +10,15 @@ using System.Net;
 
 public class NetworkMan : MonoBehaviour
 {
-    public UdpClient udp; // 소켓
+    public UdpClient udp; // Socket
 
     // Start is called before the first frame update
     void Start()
     {
         //create new udp and bind the ip and port
-        udp = new UdpClient(); // 소켓초기화
+        udp = new UdpClient(); // Initailing socket
 
-        udp.Connect("3.20.240.191", 12345); // AWS - SERVER
+        udp.Connect("3.20.240.191", 12345); // AWS - SERVER _ Junho Kim
         //udp.Connect("localhost", 12345); //  LOCAL
 
         //send msg to server - need to send bytes -- Encoding.ASCII.GetBytes -> converting
@@ -28,7 +28,7 @@ public class NetworkMan : MonoBehaviour
         udp.BeginReceive(new AsyncCallback(OnReceived), udp);
 
         InvokeRepeating("HeartBeat", 1, 1); //every one second, running heartbeat
-        InvokeRepeating("SendPosition", 0.3f, 0.3f); // 0.3초마다 send position 함수 호출
+        InvokeRepeating("SendPosition", 0.3f, 0.3f); // call send position function every 0.3 seconds
     }
 
     void OnDestroy()
@@ -72,17 +72,21 @@ public class NetworkMan : MonoBehaviour
         public Player[] players;
 
     }
-
-    [Serializable] // Json string 을 각각의 클래스로 형변환 할때 필요함.반대로, Json으로 묶어서 서버에 줄때도 이게 있어야 Json 클래스로 만들 수 있음.
+    // Json string 을 각각의 클래스로 형변환 할때 필요함.반대로, Json으로 묶어서 서버에 줄때도 이게 있어야 Json 클래스로 만들 수 있음.
+    // -> to convert json string to class, I need [Serializable]. In the opposite way, also need this
+    [Serializable] 
     public class AllClientsInfo
     {
-        public IDColorPos[] players; // 서버랑 클라가 데이터를 주고받을때 변수이름, 서버에서 보내는 이름이 같아야함.
-                                 // IDColor[] -  ID, Color가 들어있음.
+        // 서버랑 클라가 데이터를 주고받을때 변수이름, 서버에서 보내는 이름이 같아야함.
+        // -> all the variables name should be same with server!!!!!
+        public IDColorPos[] players;  
     }
 
     [Serializable]
-    public class IDColorPos // 서버로부터 ID, Color를 받아옴
-    {
+    public class IDColorPos // get id, color, position from server
+    {   
+        //***************************************** very important!!!!!!!!!!!!!!!!!!!!!!! *********************************
+        //dictionary key and variable name should be same!!!!!!!!!!!!!!!!!!!!!!! --> even upper and lower!!!!!!!!!
         public string id;
         [Serializable]
         public struct RGB
@@ -107,45 +111,55 @@ public class NetworkMan : MonoBehaviour
     public GameState lastestGameState;
     public AllClientsInfo lastestInfo;
     public AllClientsInfo allClientsInfo;
-    //dictionary key값이랑 변수 이름이랑 같아야함.
     public IDColorPos idFromServer;
 
 
     public Player player;
     public GameObject capsule;
     public GameObject me;
-    Dictionary<string, GameObject> listOfPlayer = new Dictionary<string, GameObject>(); // string - ip, port gameObject - capsule
+    // string - ip, port gameObject - capsule --> change this to cube at the last
+    Dictionary<string, GameObject> listOfPlayer = new Dictionary<string, GameObject>(); 
 
     bool needSpawn = false;
     bool needSpawn2 = false;
 
     // 클라가 서버로부터 정보를 받는곳
-    void OnReceived(IAsyncResult result)
-    { // result  = socket
-        UdpClient socket = result.AsyncState as UdpClient; // 소켓 받은거 소켓으로 형변환
+    // clients get information from server
+    void OnReceived(IAsyncResult result) // result  = socket
+    { 
+       
+        UdpClient socket = result.AsyncState as UdpClient; // result convert to udpclient's socket
 
-        IPEndPoint source = new IPEndPoint(0, 0); // 누가 보냈는지
+        IPEndPoint source = new IPEndPoint(0, 0); // sent by whom? or send to where? idk
 
-        // EndReceive --> get data,  return byte[] // EndReceive 로 자료를 수신하고 다시 BeginReceive 호출하여 자료 수신 대기, 다은손님 못들어오게 끊음 일단 
-        byte[] message = socket.EndReceive(result, ref source);  // 파라미터 - 서버의 정보
+        // EndReceive 로 자료를 수신하고 다시 BeginReceive 호출하여 자료 수신 대기, 다은손님 못들어오게 끊음 일단 
+        // EndReceive --> get data,  return byte[] --> send 
 
-        //서버의 정보를 converting to string
+        // 파라미터 - 서버의 정보
+        // -> parameter = information of server
+        byte[] message = socket.EndReceive(result, ref source);
+
+        // convert server information to string
         string returnData = Encoding.ASCII.GetString(message);
         //Debug.Log("Got this: " + returnData);
 
-        latestMessage = JsonUtility.FromJson<Message>(returnData);  // FromJson: 서버에서 보낸 Json data를 우리가 알수있게 바꿈. <Message> 클래스로형변환
+        // FromJson: 서버에서 보낸 Json data를 우리가 알수있게 바꿈. <Message> 클래스로형변환
+        // -> FromJson: convert Json data from server --> to Message class
+        latestMessage = JsonUtility.FromJson<Message>(returnData);  
         try
         {
             // 멀티쓰레딩을 사용해서, 여기서는 유니티 함수를 사용 할 수없음.
+            // -> because of multi threading, I cannot use unity method here.
             switch (latestMessage.cmd)
-            { // latestMessage의 cmd가 무엇인지 
-                case commands.NEW_CLIENT: // 기존의 클라들이 새로운 클라들의 정보를 받음
+            { 
+                // latestMessage의 cmd -> enum 
+                case commands.NEW_CLIENT: // old clients get new client's info
                     needSpawn2 = true;
                     player = JsonUtility.FromJson<Player>(returnData);
                     break;
 
                 case commands.UPDATE:
-                    allClientsInfo = JsonUtility.FromJson<AllClientsInfo>(returnData); // 기존의 클라들의 색, id가 들어있다.
+                    allClientsInfo = JsonUtility.FromJson<AllClientsInfo>(returnData); // contains old clients' color id and position
 
                     for (int i = 0; i < allClientsInfo.players.Length; ++i)
                     {
@@ -182,7 +196,7 @@ public class NetworkMan : MonoBehaviour
         socket.BeginReceive(new AsyncCallback(OnReceived), socket);
     }
 
-    // 나에게 두성이와 현교를 보여줌
+    // show me old clients
     void SpawnOldClients()
     {
         if(needSpawn == true)
@@ -199,13 +213,12 @@ public class NetworkMan : MonoBehaviour
         }
     }
 
-    // 두성이와 현교에게 나를 보여줌
+    // old clients can see me 
     void SpawnPlayers()
     {
         if(needSpawn2 == true)
         {
             needSpawn2 = false;
-            //gm 에 instantiating 할거 넣어줌
             GameObject gm = Instantiate(capsule, Vector3.zero, Quaternion.identity);
 
             //  Dictionary<string, GameObject> listOfPlayer에 player의 id = ip, port와 만들거 넣어줌
